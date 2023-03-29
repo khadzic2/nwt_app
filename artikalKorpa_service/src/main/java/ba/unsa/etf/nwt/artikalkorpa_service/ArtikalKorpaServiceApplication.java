cc20b734cc392801feb683cbaa85adbbfece1674
dahmic1907 <dahmic3@etf.unsa.ba>
2023-03-30 00:46:04 +0200

    Initial commit

---
+++ artikalKorpa_service/src/main/java/ba/unsa/etf/nwt/artikalkorpa_service/ArtikalKorpaServiceApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.artikalkorpa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtikalKorpaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtikalKorpaServiceApplication.class, args);
+    }
+
+}
---
+++ artikalKorpa_service/src/test/java/ba/unsa/etf/nwt/artikalkorpa_service/ArtikalKorpaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.artikalkorpa_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtikalKorpaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
