43a3506c47a398a1c4db84231e26b61a5bae289f
dahmic1907 <73670063+dahmic1907@users.noreply.github.com>
2023-05-25 23:53:08 +0200

    Revert "Item-cart and user service"

--- itemCart_service/src/main/java/ba/unsa/etf/nwt/itemcart_service/repository/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.nwt.itemcart_service.repository;
-
-import ba.unsa.etf.nwt.itemcart_service.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart,Integer> {
-}
--- itemCart_service/src/main/java/ba/unsa/etf/nwt/itemcart_service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.nwt.itemcart_service.repository;
-
-import ba.unsa.etf.nwt.itemcart_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
