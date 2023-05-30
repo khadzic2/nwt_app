a91308ed9b35e9fd3ad7db8d3ab09ba12869fbc5
shebibovic <sanida.hebibovic@edu.fit.ba>
2023-05-30 23:23:25 +0200

    added sync and image model

---
+++ item_service/src/main/java/ba/unsa/etf/nwt/item_service/Model/Specifications.java
@@ -0,0 +1,63 @@
+package ba.unsa.etf.nwt.item_service.Model;
+
+import jakarta.persistence.*;
+
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+public class Specifications {
+    @NotNull(message="Can't be null!")
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String color;
+    private Double weight;
+    private String dimensions;
+    private String material;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public Specifications(){
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public Item getItem(){ return item;}
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
