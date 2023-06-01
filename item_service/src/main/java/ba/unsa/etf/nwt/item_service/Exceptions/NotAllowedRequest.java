2c4c945f1e3520a29b88930017f9232ec90f31ef
Kanita Hadzic <khadzic2@etf.unsa.ba>
2023-06-01 03:06:13 +0200

    item service deleted

--- item_service/src/main/java/ba/unsa/etf/nwt/item_service/Exceptions/NotAllowedRequest.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.nwt.item_service.Exceptions;
-
-public class NotAllowedRequest extends RuntimeException{
-    public NotAllowedRequest(String message) {
-        super(message);
-    }
-}
--- item_service/src/main/java/ba/unsa/etf/nwt/item_service/Repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package ba.unsa.etf.nwt.item_service.Repository;
-
-import ba.unsa.etf.nwt.item_service.Model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ImageRepository extends JpaRepository<Image,Integer> {
-    Optional<Image> findByName(String fileName);
-}
