c7ec9b7925d2cf3202238243b579d181e4304d09
Kanita Hadzic <khadzic2@etf.unsa.ba>
2023-06-01 12:54:13 +0200

    item service added

---
+++ item_service/src/main/java/ba/unsa/etf/nwt/item_service/Controller/ItemController.java
@@ -0,0 +1,93 @@
+package ba.unsa.etf.nwt.item_service.Controller;
+
+
+import java.util.List;
+
+import ba.unsa.etf.nwt.item_service.Exceptions.NotAllowedRequest;
+import ba.unsa.etf.nwt.item_service.Exceptions.NotFoundException;
+import ba.unsa.etf.nwt.item_service.Model.Item;
+import ba.unsa.etf.nwt.item_service.Repository.ItemRepository;
+import ba.unsa.etf.nwt.item_service.Service.ItemService;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import ba.unsa.etf.nwt.item_service.Exceptions.RestExceptionHandler;
+
+@RestController
+@RequestMapping(value = "/items", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ItemController {
+
+    private final ItemRepository repository;
+    private ItemService itemService;
+    RestTemplate restTemplate;
+
+    ItemController(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/items")
+    List<Item> all() {
+        return repository.findAll();
+    }
+
+
+    @PostMapping("/item")
+    Item newItem(@RequestBody Item item) {
+        return repository.save(item);
+    }
+
+    @GetMapping("/item/{id}")
+    Item one(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException(id));
+    }
+
+    @GetMapping(path = "/items_in_cart")
+    public @ResponseBody Iterable<Item> getAllItems(){
+        return itemService.getAllItems();
+    }
+
+    @PostMapping("/add")
+    public Item AddToCart(int cartId, Item i){
+        if (i.getStatus().equals(Item.StatusType.NOT_AVAILABLE)) throw new
+                NotAllowedRequest("Item is not available");
+
+        Item item = restTemplate.postForObject("user/cart/"+cartId, i, i.getClass());
+        return item;
+        ///api/cart
+    }
+
+    public void checkStock(String itemId) {
+        String url = "http://stock-service-api.com/stock/{itemId}";
+
+        ResponseEntity<Integer> response =
+                restTemplate.exchange(url, HttpMethod.GET, null, Integer.class, itemId);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            int stockQuantity = response.getBody();
+            System.out.println("Item stock quantity: " + stockQuantity);
+        } else {
+            System.out.println("Failed to retrieve item stock quantity.");
+        }
+    }
+
+
+    @GetMapping("/items/cart/{cart_id}")
+    public ResponseEntity<List<Item>> getItemsFromCart(@PathVariable Integer id){
+        Item item = itemService.getItemsFromCart(id).get(0);
+        System.out.println(itemService.getItemsFromCart(id));
+        //return null;
+        return ResponseEntity.ok(itemService.getItemsFromCart(id));
+    }
+
+    @GetMapping("/api/item/{id}/days")
+    ResponseEntity<Item> getDaysByItem(@PathVariable Integer id) {
+        return new ResponseEntity<>(itemService.getDaysByItem(id), HttpStatus.OK);
+    }
+
+
+}
---
+++ item_service/src/main/java/ba/unsa/etf/nwt/item_service/Model/ItemCategory.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.nwt.item_service.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class ItemCategory {
+    @NotNull(message = "Can't be null!")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String category_name;
+
+
+    @OneToMany(mappedBy = "itemCategory")
+    @JsonIgnoreProperties("itemCategory")
+    private List<Item> items;
+
+    public ItemCategory() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+}
