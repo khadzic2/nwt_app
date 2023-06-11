e92566a610466b6bed0fa7316f72ed717e26404b
dahmic1907 <dahmic3@etf.unsa.ba>
2023-06-11 20:56:34 +0200

    Remove stock

--- item_service/src/main/java/ba/unsa/etf/nwt/item_service/Repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.nwt.item_service.Repository;
-
-import ba.unsa.etf.nwt.item_service.Model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock,Integer> {
-}
